0f3de6f079a000708c7d6a35e39b23683343edf0
Mohammed Ly <lybhoye@yahoo.com>
2024-06-13 17:19:37 +0200

    Initial Commit for the test repository

--- src/main/java/pullPushInGitHub/Main.java
+++ src/main/java/pullPushInGitHub/Main.java
@@ -3,8 +3,12 @@ package pullPushInGitHub;
 public class Main {
     public static void main(String[] args) {
         UebueMitGitHubUndGit ub = new UebueMitGitHubUndGit();
-      int addme =  ub.addNumber(7,99);
-        System.out.println(addme);
+      int address =  ub.addNumber(7,99);
+        System.out.println(address);
+        int multply =  ub.multiply(7,9);
+
+        System.out.println(address);
+        System.out.println(multply);
 //      ub.rechner('+', 34, 66);
 
        //ub.rechner('*', 7,9);
--- src/main/java/pullPushInGitHub/UebueMitGitHubUndGit.java
+++ src/main/java/pullPushInGitHub/UebueMitGitHubUndGit.java
@@ -5,5 +5,7 @@ public class UebueMitGitHubUndGit {
     public int addNumber(int a, int b){
         return a + b;
     }
-
+public  int multiply(int a, int b){
+        return a * b;
+}
 }
