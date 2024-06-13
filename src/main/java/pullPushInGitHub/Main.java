283e87714b5d7bb82f299e8fed950fcbfa600a75
Mohammed Ly <lybhoye@yahoo.com>
2024-06-13 17:07:42 +0200

    Initial Commit for the test repository

---
+++ src/main/java/pullPushInGitHub/Main.java
@@ -0,0 +1,14 @@
+package pullPushInGitHub;
+
+public class Main {
+    public static void main(String[] args) {
+        UebueMitGitHubUndGit ub = new UebueMitGitHubUndGit();
+      int addme =  ub.addNumber(7,99);
+        System.out.println(addme);
+//      ub.rechner('+', 34, 66);
+
+       //ub.rechner('*', 7,9);
+      // ub.rechner('-', 77, 23);
+
+    }
+}
---
+++ src/main/java/pullPushInGitHub/UebueMitGitHubUndGit.java
@@ -0,0 +1,9 @@
+package pullPushInGitHub;
+
+public class UebueMitGitHubUndGit {
+
+    public int addNumber(int a, int b){
+        return a + b;
+    }
+
+}
